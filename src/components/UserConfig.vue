b7060207e7530d4c1ebea7e06e14402059cfe3da
Jonatan Cardoso <jonatancardosoc@gmail.com>
2024-01-28 22:49:45 -0400

    PUT in LocalStorage - New User

--- src/components/UserConfig.vue
+++ src/components/UserConfig.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <div class="mt-10 mx-20">
       <div class="border-b border-gray-900/10 pb-12">
         <h2 class="text-base font-semibold leading-7 text-gray-900">Informações do Usuário</h2>
@@ -9,57 +9,102 @@
           <div class="sm:col-span-3">
             <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Nome *</label>
             <div class="mt-1">
-              <input type="text" name="name" id="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input type="text" name="name" id="name" v-model="name" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
           <div class="sm:col-span-3">
-            <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Documento (CPF) *</label>
+            <label for="document" class="block text-sm font-medium leading-6 text-gray-900">Documento (CPF) *</label>
             <div class="mt-1">
-              <input type="text" name="document" id="document" required v-mask="'###.###.###-##'" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input v-mask="'###.###.###-##'" type="text" v-model="document" name="document" id="document" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
           <div class="sm:col-span-3">
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Senha *</label>
             <div class="mt-1">
-              <input id="password" name="password" type="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input id="password" name="password" type="password" v-model="password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
           
           <div class="sm:col-span-3">
-            <label for="confirm-password" class="block text-sm font-medium leading-6 text-gray-900">Confirmar Senha *</label>
+            <label for="confirmPassword" class="block text-sm font-medium leading-6 text-gray-900">Confirmar Senha *</label>
             <div class="mt-1">
-              <input id="confirm-password" name="confirm-password" required type="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <input id="confirmPassword" name="confirmPassword" required type="password"  v-model="confirmPassword" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
         </div>
 
         <div class="border-b border-gray-900/10 pb-5">
           <fieldset class="mt-5">
-            <legend class="text-sm font-semibold leading-6 text-gray-900">Status de usuário</legend>
             <div class="sm:col-span-3">
-              <div class="flex items-center gap-x-3">
-                <input id="active" name="push-status" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                <label for="active" class="block text-sm font-medium leading-6 text-gray-900">Ativo</label>
-              </div>
-              <div class="flex items-center gap-x-3">
-                <input id="inative" name="push-status" type="radio" class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600" />
-                <label for="inative" class="block text-sm font-medium leading-6 text-gray-900">Inativo</label>
-              </div>
+              <SwitchGroup as="div" class="flex gap-x-4 sm:col-span-2">
+                <div class="flex h-6 items-center">
+                  <Switch v-model="isActive" :class="[isActive ? 'bg-indigo-600' : 'bg-gray-200', 'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600']">
+                    <span aria-hidden="true" :class="[isActive ? 'translate-x-3.5' : 'translate-x-0', 'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out']" />
+                  </Switch>
+                  <legend class="ml-5 text-sm leading-6 text-gray-900">Status de usuário</legend>
+                </div>
+              </SwitchGroup>
             </div>
           </fieldset>
         </div>
 
         <div class="mt-6 flex items-center justify-end">
-          <button type="button" class="mx-2 text-sm font-semibold leading-6 text-gray-900">Voltar</button>
+          <button @click.prevent="goBack()" type="button" class="mx-2 text-sm font-semibold leading-6 text-gray-900">Voltar</button>
           <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Salvar</button>
         </div>
       </div>
     </div>
   </form>
 </template>
+<script setup lang="ts">
+import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue';
 
-<script setup>
-import { PhotoIcon, UserCircleIcon } from '@heroicons/vue/24/solid'
+interface User {
+  name: string;
+  document: string;
+  password: string;
+  isActive: boolean;
+}
+
+const storedUsers = localStorage.getItem('users');
+const initialUsers = storedUsers ? JSON.parse(storedUsers) : [];
+
+const users = ref<Array<User>>(initialUsers);
+
+const name = ref('');
+const document = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+const isActive = ref(true);
+
+const handleSubmit = () => {
+  const newUser: User = {
+    name: name.value,
+    document: document.value,
+    password: password.value,
+    isActive: isActive.value,
+  };
+
+  users.value.push(newUser);
+
+  // Salvando no localStorage
+  localStorage.setItem('users', JSON.stringify(users.value));
+
+  // Limpando os campos do formulário
+  name.value = '';
+  document.value = '';
+  password.value = '';
+  confirmPassword.value = '';
+  isActive.value = true;
+};
+
+const router = useRouter()
+
+const goBack = () => {
+  router.push({ name: 'users' })
+};
 </script>
