365f9f77e4565b895139e56fb8c7a24ebdd5357d
Jonatan Cardoso <jonatancardosoc@gmail.com>
2024-01-29 13:47:21 -0400

    PUT in LocalStorage - userId

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,67 +1,113 @@
 <template>
-    <div>
-        <ul role="list" class="divide-y divide-gray-100 mx-10">
-            <li v-for="(person, index) in users" :key="person.id" class="flex justify-between gap-x-6 py-5">
-            <div class="flex min-w-0 gap-x-4">
-                <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
-                <div class="min-w-0 flex-auto">
-                    <p class="text-sm font-semibold leading-6 text-gray-900">{{ person.name }}</p>
-                    <div v-if="person.isActive !== true" class="mt-1 flex items-center gap-x-1.5">
-                        <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                            <div class="h-1.5 w-1.5 rounded-full bg-red-500" />
-                        </div>
-                        <p class="text-xs leading-5 text-gray-500">Inativo</p>
-                    </div>
-                    <div v-else class="mt-1 flex items-center gap-x-1.5">
-                        <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                            <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                        </div>
-                        <p class="text-xs leading-5 text-gray-500">Ativo</p>
-                    </div>
-                </div>
-                
+  <div>
+    <ul role="list" class="divide-y divide-gray-100 mx-10">
+      <li
+        v-for="(person, index) in users"
+        :key="person.id"
+        class="flex justify-between gap-x-6 py-5"
+      >
+        <div class="flex min-w-0 gap-x-4">
+          <img
+            class="h-12 w-12 flex-none rounded-full bg-gray-50"
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            alt=""
+          />
+          <div class="min-w-0 flex-auto">
+            <p class="text-sm font-semibold leading-6 text-gray-900">{{ person.name }}</p>
+            <div v-if="person.isActive !== true" class="mt-1 flex items-center gap-x-1.5">
+              <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                <div class="h-1.5 w-1.5 rounded-full bg-red-500" />
+              </div>
+              <p class="text-xs leading-5 text-gray-500">Inativo</p>
             </div>
-            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                <Menu as="div" class="relative inline-block text-left">
-                    <div>
-                    <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
-                        <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    </MenuButton>
-                    </div>
+            <div v-else class="mt-1 flex items-center gap-x-1.5">
+              <div class="flex-none rounded-full bg-emerald-500/20 p-1">
+                <div class="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+              </div>
+              <p class="text-xs leading-5 text-gray-500">Ativo</p>
+            </div>
+          </div>
+        </div>
+        <div class="hidden sm:flex sm:flex-col sm:items-end">
+          <Menu as="div" class="relative inline-block text-left">
+            <div>
+              <MenuButton
+                class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+                <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+              </MenuButton>
+            </div>
 
-                    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                        <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <div class="py-1">
-                                <MenuItem v-slot="{ active }">
-                                    <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Editar</a>
-                                </MenuItem>
-                                <MenuItem  @click="() => deleteUser(index)" v-slot="{ active }">
-                                    <a href="#" :class="[active ? 'bg-gray-100 text-red-600' : 'text-red-600', 'block px-4 py-2 text-sm']">Deletar</a>
-                                </MenuItem>
-                            </div>
-                        </MenuItems>
-                    </transition>
-                </Menu>
-            </div>
-            </li>
-            <div class="mt-6 flex items-center justify-end">
-              <button @click="newUser" class="mt-5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Novo Usuário</button>
-            </div>
-        </ul>
-    </div>
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a @click.prevent="navigateToEditUser(person.id)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">Editar</a>
+                  </MenuItem>
+                  <MenuItem @click="() => deleteUser(index)" v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100 text-red-600' : 'text-red-600',
+                        'block px-4 py-2 text-sm'
+                      ]"
+                      >Deletar</a
+                    >
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+      </li>
+      <div class="mt-6 flex items-center justify-end">
+        <button
+          @click="newUser"
+          class="mt-5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Novo Usuário
+        </button>
+      </div>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
 import { ChevronDownIcon } from '@heroicons/vue/20/solid';
-import { useRouter } from 'vue-router'
+import { useRouter } from 'vue-router';
 
 const users = ref([]);
+const localStorageKey = 'editUserId'; // Nome da chave no localStorage
 
 onMounted(async () => {
   const storedUsers = localStorage.getItem('users');
@@ -73,8 +119,13 @@ const deleteUser = (index) => {
   localStorage.setItem('users', JSON.stringify(users.value));
 };
 
-const router = useRouter()
+const router = useRouter();
 const newUser = () => {
-    router.push({ name: 'user' })
-}
+  router.push({ name: 'user' });
+};
+
+const navigateToEditUser = (userId) => {
+  localStorage.setItem(localStorageKey, userId.toString());
+  router.push({ name: 'user', params: { id: userId } });
+};
 </script>
