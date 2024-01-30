8e40e06addd12aac0814786ade83ed475c5c7773
Jonatan Cardoso <jonatancardosoc@gmail.com>
2024-01-30 00:33:50 -0400

    Install notifications alert

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
+import Notifications from '@kyvg/vue3-notification'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(Notifications)
 
 app.mount('#app')
