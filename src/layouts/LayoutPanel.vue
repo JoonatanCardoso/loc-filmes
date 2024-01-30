64cb0148df905f73eceec6247d179dfdd2d720a3
Jonatan Cardoso <jonatancardosoc@gmail.com>
2024-01-30 10:51:13 -0400

    Adding logout in button desktop

--- src/layouts/LayoutPanel.vue
+++ src/layouts/LayoutPanel.vue
@@ -67,14 +67,11 @@
                   <MenuItems
                     class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                   >
-                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                      <a
-                        :href="item.href"
-                        :class="[
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        ]"
-                        >{{ item.name }}</a
+                    <MenuItem>
+                      <DisclosureButton
+                        @click.prevent="logout()"
+                        class="w-full block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-500 hover:text-white"
+                        >Sair</DisclosureButton
                       >
                     </MenuItem>
                   </MenuItems>
