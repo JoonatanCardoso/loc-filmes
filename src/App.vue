d9f92a01867babade7c895f5df0782fb907a6000
Jonatan Cardoso <jonatancardosoc@gmail.com>
2024-01-30 10:50:33 -0400

    Created user admin

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
-  <script setup lang="ts">
-  import { RouterView } from 'vue-router';
-  </script>
-  
 <template>
   <RouterView name="layout" />
   <RouterView />
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+interface User {
+  id: number
+  name: string
+  email: string
+  document: string
+  password: string
+  isActive: boolean
+}
+
+const users = ref([]);
+
+
+onMounted(() => {
+  const adminUser: User = {
+    id: 1,
+    name: 'admin',
+    email: 'admin@admin.com',
+    document: '123456789',
+    password: 'admin',
+    isActive: true
+  }
+
+  const storedUsers = localStorage.getItem('users')
+  const users = storedUsers ? JSON.parse(storedUsers) : []
+
+  const adminExists = users.some((user: User) => user.email === adminUser.email)
+
+  if (!adminExists) {
+    users.push(adminUser)
+    localStorage.setItem('users', JSON.stringify(users))
+  }
+
+})
+</script>
